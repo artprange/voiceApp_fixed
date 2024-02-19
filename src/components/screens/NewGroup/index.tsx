7a984500fe18969aa1d59ddebfaa3d378a13cbc0
artprange <arthur.prange@gmail.com>
2024-02-19 20:04:12 -0300

    text input component

--- src/components/screens/NewGroup/index.tsx
+++ src/components/screens/NewGroup/index.tsx
@@ -2,6 +2,7 @@ import { Highlight } from "@components/Highlight";
 import { Container, Content, Icon } from "./styles";
 import { Header } from "@components/Header";
 import { Button } from "@components/Button";
+import { Input } from "@components/Input";
 
 export function NewGroup(){
     return(
@@ -17,8 +18,11 @@ export function NewGroup(){
                 subTitle="Crie um novo grupo para jogar com os noobs dos seus amigos!"
                 />
 
+                <Input />
+
                 <Button
                 title="Criar novo grupo"
+                style={{marginTop: 20}}
                 />
 
             </Content>
