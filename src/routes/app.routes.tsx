c306cc32ed715f24fc130d915567a9d302072e04
artprange <arthur.prange@gmail.com>
2024-03-02 18:05:16 -0300

    routes are set

---
+++ src/routes/app.routes.tsx
@@ -0,0 +1,28 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import { Groups} from "../components/screens/Groups";
+import { Players } from "../components/screens/Players";
+import { NewGroup } from "@components/screens/NewGroup";
+
+const { Navigator, Screen } = createNativeStackNavigator();
+
+export function AppRoutes() {
+	return (
+		<Navigator>
+			<Screen
+				name="groups"
+				component={Groups}
+			/>
+
+			<Screen
+				name="new"
+				component={NewGroup}
+			/>
+
+			<Screen
+				name="players"
+				component={Players}
+			/>
+		</Navigator>
+	);
+}
