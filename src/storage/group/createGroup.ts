2ff38673b1258f942a6839567b02aabc9d385728
artprange <arthur.prange@gmail.com>
2024-03-11 20:42:14 -0300

    error handling

--- src/storage/group/createGroup.ts
+++ src/storage/group/createGroup.ts
@@ -2,6 +2,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { GROUP_COLLECTION } from '@storage/storageConfig';
 
 import { getAllGroups } from './getAllGroups';
+import { AppError } from '@utils/AppError';
 
 export async function createGroup(newGroup: string){
 
@@ -9,6 +10,11 @@ export async function createGroup(newGroup: string){
     try{
 
         const storedGroups = await getAllGroups();
+
+        const groupAlreadyExists = storedGroups.includes(newGroup);
+        if(groupAlreadyExists){
+            throw new AppError("Já existe um grupo com esse nome")
+        }
 
         const storage = JSON.stringify([...storedGroups, newGroup]);
 
